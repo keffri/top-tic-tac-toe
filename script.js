be72fd300ea7398497ebc53b4f2b7e8b3af26049
keffri <keffri.w.neal@gmail.com>
2021-02-20 12:41:17 -0800

    game board and display controller

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 "use strict";
+
+const gameboard = (() => {
+  let gameBoard = {
+    gameboard: ["", "", "", "", "", "", "", "", ""],
+  };
+
+  return { gameBoard };
+})();
+
+const displayController = (() => {
+  let ticTacToeGrid = document.getElementById("ticTacToeGrid");
+
+  for (let i = 0; i < gameboard.gameBoard.gameboard.length; i++) {
+    ticTacToeGrid.children[i].textContent = gameboard.gameBoard.gameboard[i];
+  }
+
+  const ticTacToeBoxes = document.querySelectorAll(".ticTacToeBox");
+
+  let playerTurn = "X";
+
+  ticTacToeBoxes.forEach((box) => {
+    box.addEventListener("click", (e) => {
+      if (box.textContent != "") {
+        return;
+      }
+      if (playerTurn === "X") {
+        box.textContent = playerTurn;
+        playerTurn = "O";
+      } else if (playerTurn === "O") {
+        box.textContent = playerTurn;
+        playerTurn = "X";
+      }
+    });
+  });
+
+  return { ticTacToeGrid };
+})();
+
+const Player = (playerName, playerMarker) => {
+  return { playerName, playerMarker };
+};
