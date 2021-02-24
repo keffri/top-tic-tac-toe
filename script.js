32cbf519c5fd7bf042cb4082d77a232b599b9088
keffri <keffri.w.neal@gmail.com>
2021-02-23 17:16:26 -0800

    smaller  checkWinner function

--- script.js
+++ script.js
@@ -31,104 +31,36 @@ const gameboard = (() => {
     }
   };
 
-  /*
+  let winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-
-  let winningCombos = [[0, 1, 2], [3, 4, 5]]
-
-
-
-
-  */
-
-  const checkWinner = function (gameText) {
-    if (board[0] === board[1] && board[0] === board[2] && board[0] != "") {
-      gameboard.gameWon = true;
-      if (board[0] === "X") {
-        gameText.textContent = `${playerOne.playerName} wins!`;
-      } else if (board[0] === "O") {
-        gameText.textContent = `${playerTwo.playerName} wins!`;
-      }
-    } else if (
-      board[3] === board[4] &&
-      board[3] === board[5] &&
-      board[3] != ""
-    ) {
-      gameboard.gameWon = true;
-      if (board[3] === "X") {
-        gameText.textContent = `${playerOne.playerName} wins!`;
-      } else if (board[3] === "O") {
-        gameText.textContent = `${playerTwo.playerName} wins!`;
-      }
-    } else if (
-      board[6] === board[7] &&
-      board[6] === board[8] &&
-      board[6] != ""
-    ) {
-      gameboard.gameWon = true;
-      if (board[6] === "X") {
-        gameText.textContent = `${playerOne.playerName} wins!`;
-      } else if (board[6] === "O") {
-        gameText.textContent = `${playerTwo.playerName} wins!`;
-      }
-    } else if (
-      board[0] === board[3] &&
-      board[0] === board[6] &&
-      board[0] != ""
-    ) {
-      gameboard.gameWon = true;
-      if (board[0] === "X") {
-        gameText.textContent = `${playerOne.playerName} wins!`;
-      } else if (board[0] === "O") {
-        gameText.textContent = `${playerTwo.playerName} wins!`;
-      }
-    } else if (
-      board[1] === board[4] &&
-      board[1] === board[7] &&
-      board[1] != ""
-    ) {
-      gameboard.gameWon = true;
-      if (board[1] === "X") {
-        gameText.textContent = `${playerOne.playerName} wins!`;
-      } else if (board[1] === "O") {
-        gameText.textContent = `${playerTwo.playerName} wins!`;
-      }
-    } else if (
-      board[2] === board[5] &&
-      board[2] === board[8] &&
-      board[2] != ""
-    ) {
-      gameboard.gameWon = true;
-      if (board[2] === "X") {
-        gameText.textContent = `${playerOne.playerName} wins!`;
-      } else if (board[2] === "O") {
-        gameText.textContent = `${playerTwo.playerName} wins!`;
-      }
-    } else if (
-      board[0] === board[4] &&
-      board[0] === board[8] &&
-      board[0] != ""
-    ) {
-      gameboard.gameWon = true;
-      if (board[0] === "X") {
-        gameText.textContent = `${playerOne.playerName} wins!`;
-      } else if (board[0] === "O") {
-        gameText.textContent = `${playerTwo.playerName} wins!`;
-      }
-    } else if (
-      board[2] === board[4] &&
-      board[2] === board[6] &&
-      board[2] != ""
-    ) {
-      gameboard.gameWon = true;
-      if (board[2] === "X") {
-        gameText.textContent = `${playerOne.playerName} wins!`;
-      } else if (board[2] === "O") {
-        gameText.textContent = `${playerTwo.playerName} wins!`;
-      }
-    } else if (board.every(checkTie)) {
+  const checkWinner = function () {
+    if (board.every(checkTie)) {
       gameText.textContent = `Match tie.`;
+      return;
     }
+    winningCombos.forEach((combo) => {
+      if (
+        board[combo[0]] === board[combo[1]] &&
+        board[combo[0]] === board[combo[2]] &&
+        board[combo[0]]
+      ) {
+        gameboard.gameWon = true;
+        if (board[combo[0]] === "X") {
+          gameText.textContent = `${playerOne.playerName} wins!`;
+        } else if (board[combo[0]] === "O") {
+          gameText.textContent = `${playerTwo.playerName} wins!`;
+        }
+      }
+    });
   };
 
   const checkTie = (cv) => cv != "";
@@ -141,6 +73,7 @@ const gameboard = (() => {
     checkWinner,
     resetBoard,
     gameWon,
+    winningCombos,
   };
 })();
 
